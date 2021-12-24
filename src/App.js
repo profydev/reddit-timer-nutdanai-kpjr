ebef3edb7c6ef3da6f1019fdff3fe99a950d7c85
nutdanai-kpjr <nutdanai.282@mail.kmutt.ac.th>
2021-12-24 14:57:42 +0700

    Add globalstyle && Add router paths

--- src/App.js
+++ src/App.js
@@ -1,10 +1,45 @@
 import React from 'react';
-import './App.css';
+import './globalStyles.css';
+import {
+  BrowserRouter as Router, Routes, Route, Link,
+} from 'react-router-dom';
 
 function App() {
   return (
+    <Router>
+      <div>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/search">Search</Link>
+          </li>
+
+        </ul>
+
+        <hr />
+
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route path="/search" element={<Search />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+function Home() {
+  return (
     <div>
-      <h1>Test</h1>
+      <h2>Home</h2>
+    </div>
+  );
+}
+
+function Search() {
+  return (
+    <div>
+      <h2>Search</h2>
     </div>
   );
 }
