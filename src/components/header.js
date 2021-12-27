0c4a117b53448f374ecd0456c486ab76e8c24740
nutdanai-kpjr <nutdanai.282@mail.kmutt.ac.th>
2021-12-27 13:40:59 +0700

    Update folder structure

---
+++ src/components/header.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Logo from '../images/logo.svg';
+import './header.css';
+
+function Header() {
+  return (
+    <header>
+      <nav>
+        <div className="align-left">
+          <Link className="logo-link" to="/">
+            <img src={Logo} alt="logo" />
+          </Link>
+        </div>
+        <div className="align-right">
+          <Link to="/search/javascript">Search</Link>
+          <Link to="#how-it-works">How it works</Link>
+          <Link to="#about ">About</Link>
+        </div>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
